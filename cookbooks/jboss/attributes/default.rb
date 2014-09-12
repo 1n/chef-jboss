c101a9ba52c3b2211c4c222209f46aa67046a00a
Vladislav Tkatchev <vlad.tkatchev@gmail.com>
2014-09-12 15:49:03 +0300

    jboss installed

---
+++ cookbooks/jboss/attributes/default.rb
@@ -0,0 +1,4 @@
+default['jboss']['jboss_package'] = "/tmp/jboss.zip"
+default['jboss']['jboss_url'] = "http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip"
+default['jboss']['jboss_home'] = "/opt"
+default['jboss']['jboss_user'] = "jboss-as"
--- cookbooks/jboss/recipes/default.rb
+++ cookbooks/jboss/recipes/default.rb
@@ -6,3 +6,42 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+remote_file "#{node['jboss']['jboss_package']}" do
+	source "#{node['jboss']['jboss_url']}"
+	checksum "0aece7899b54"
+end
+
+yum_package "unzip"
+
+execute "unzip" do
+	command "unzip #{node['jboss']['jboss_package']} -d #{node['jboss']['jboss_home']}"
+	not_if { ::File.exists?("#{node['jboss']['jboss_home']}/jboss-as-7.1.1.Final")}
+end
+
+user "#{node['jboss']['jboss_user']}"
+
+link "/opt/jboss" do
+	to "#{node['jboss']['jboss_home']}/jboss-as-7.1.1.Final"
+	owner "#{node['jboss']['jboss_user']}"
+	group "#{node['jboss']['jboss_user']}"
+end
+
+jboss_home = "#{node['jboss']['jboss_home']}/jboss"
+
+execute "chown /opt/jboss" do
+	command "chown -RL #{node['jboss']['jboss_user']}.#{node['jboss']['jboss_user']} #{jboss_home}"
+end
+
+cookbook_file "jboss-as-standalone.sh" do
+	path "/etc/init.d/jboss"
+	owner "root"
+	group "root"
+	mode "0755"
+end
+
+cookbook_file "jboss-as.conf" do
+	path "/etc/jboss-as/jboss-as.conf"
+	owner "root"
+	group "root"
+end
